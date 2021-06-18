8e31b6dd851b859f6f1286dfd577511bc74ef65e
Willake Linode <willakelin@gmail.com>
2021-06-18 00:58:12 +0000

    update token list

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,25 @@ import * as TelegramBot from "node-telegram-bot-api";
 import { PublicKey } from "@solana/web3.js";
 import { config } from "dotenv";
 import { PrivateClient } from "./privateClient";
+import { logAllTokensInJson } from "./utils/tokens";
 
 config();
 
-const MY_FUND_ID = new PublicKey(process.env.MY_FUND_ID);
-const TARGET_FUND_ID = new PublicKey(process.env.TARGET_FUND_ID);
+// const MY_FUND_ID = new PublicKey(process.env.MY_FUND_ID);
+// const TARGET_FUND_ID = new PublicKey(process.env.TARGET_FUND_ID);
 
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const CHAT_ID = process.env.CHAT_ID;
+// const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+// const CHAT_ID = process.env.CHAT_ID;
 
-const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
+// const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
 
-const client = new PrivateClient(
-  MY_FUND_ID,
-  TARGET_FUND_ID,
-  bot,
-  CHAT_ID
-);
+// const client = new PrivateClient(
+//   MY_FUND_ID,
+//   TARGET_FUND_ID,
+//   bot,
+//   CHAT_ID
+// );
 
-client.activate();
+// client.activate();
+
+logAllTokensInJson();
--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1,205 +1,185 @@
 const TOKENMAPS : Token[] = [
     {
-        fullName: 'USDC',
-        name: 'usdc',
-        mint: '2aqBRavq4UxeQys6ABue8zWxaJZAB9sbBRNFY3kX7p31'
-    },
-    { 
-        fullName: 'Bitcoin',
-        name: 'btc', 
-        mint: 'FYaRg2Di29JALi5B5YtAvQahJZLbZDVAFYW3JmpMrWW' 
-    },
-    { 
-        fullName: 'Ethereum',
-        name: 'eth', 
-        mint: 'AHEAwFN1qxoNMkcsRopYikgSRhiKkKUjcVux5qCBcxkj' 
-    },
-    { 
-        fullName: 'Ripple',
-        name: 'xrp', 
-        mint: '7yBAhJdvH8wtWp4mdPYNj1MPiCCdqDfG7ge4u9imPcnw' 
-    },
-    { 
-        fullName: 'Solana',
-        name: 'sol', 
-        mint: 'Csd6HUGPaenA3Kwhms2pm6pR3hJ7hdh4H2ejF9hKykkY' 
-    },
-    { 
-        fullName: 'Serum',
-        name: 'srm', 
-        mint: 'Pr3EmS5XYWwo8R28xXYQv3fLDudQFv9jvMmHsqHPwES' 
+        "fullName":"USDC",
+        "name":"usdc",
+        "mint":"2aqBRavq4UxeQys6ABue8zWxaJZAB9sbBRNFY3kX7p31"
     },
     {
-        fullName: 'Sushi',
-        name: 'sushi',
-        mint: 'GQQJXvQfzGUFfzJcLjkyM2Ziq3snmE4VFVcV2JUKcJrM'
+        "fullName":"Bitcoin",
+        "name":"btc",
+        "mint":"FYaRg2Di29JALi5B5YtAvQahJZLbZDVAFYW3JmpMrWW"
     },
     {
-        fullName: 'Link',
-        name: 'link',
-        mint: '5QSqnXtuiRxLafC1zqq4DqydFDzxsDjokCJSqF8hB5vZ'
-    },
-    { 
-        fullName: 'FTX Token',
-        name: 'ftt', 
-        mint: 'A6TviEuVTijiihS4SbFS9Y9eZbT8PPk84fBQ4xuAbjJR' 
-    },
-    { 
-        fullName: 'Binance Coin',
-        name: 'bnb', 
-        mint: '4C4HbpFFKrDjBYaRue6yxW5vT6d9Rz7baYXoB6MzXMFP' 
-    },
-    { 
-        fullName: 'Polkdot',
-        name: 'dot', 
-        mint: '21QeB8g5scwR1PbYoUfYJmkkx22Uu7fc1GPxgypWi7eK' 
-    },
-    { 
-        fullName: 'Bitcoin Cash',
-        name: 'bch', 
-        mint: 'FL84oLhoNAJQQNVtSd3sJBLbN3ov5wxyGbe8UTiAD9ZR' 
-    },
-    { 
-        fullName: 'Cardano',
-        name: 'ada', 
-        mint: 'PmpH2ay2kcQdYdS4SsUdNe4KJfPJqQc2hCdzEQf4Avy' 
-    },
-    { 
-        fullName: 'Internet Computer',
-        name: 'icp', 
-        mint: 'BVb7R64HHuKYPwkGhCUxQxxsxpzaUKcuxsDbMUDfLpbs' 
+        "fullName":"Ethereum",
+        "name":"eth",
+        "mint":"AHEAwFN1qxoNMkcsRopYikgSRhiKkKUjcVux5qCBcxkj"
     },
     {
-        fullName: 'PancakeSwap',
-        name: 'cake',
-        mint: '88iaqzemdz7uBvCgp8YBGP9Hf394SuKbnGGDNRxRuGdH'
+        "fullName":"Ripple",
+        "name":"xrp",
+        "mint":"7yBAhJdvH8wtWp4mdPYNj1MPiCCdqDfG7ge4u9imPcnw"
     },
     {
-        fullName: 'THORChain',
-        name: 'rune',
-        mint: '2qFcXo8G7XkRXFPMP96shumCvjcae9jymonFudMCAeuW'
+        "fullName":"Solana",
+        "name":"sol",
+        "mint":"Csd6HUGPaenA3Kwhms2pm6pR3hJ7hdh4H2ejF9hKykkY"
     },
     {
-        fullName: 'Unisocks',
-        name: 'socks',
-        mint: 'EfA5bnaNnABPrVigAMnE3gTLDoFC2WwEpx1ekrLkMNG2'
+        "fullName":"Serum",
+        "name":"srm",
+        "mint":"Pr3EmS5XYWwo8R28xXYQv3fLDudQFv9jvMmHsqHPwES"
     },
     {
-        fullName: 'Dogecoin',
-        name: 'doge',
-        mint: 'At3cSnA8VicjcwvLe63RayUwmNc4sVxcSwLTAdNu7f16'
+        "fullName":"Sushi",
+        "name":"sushi",
+        "mint":"GQQJXvQfzGUFfzJcLjkyM2Ziq3snmE4VFVcV2JUKcJrM"
     },
     {
-        fullName: 'Shiba Inu',
-        name: 'shib',
-        mint: 'EuD7YxbqJ7hidTGsnHT79H54SGshZx1dw4gmWf2LfcPx'
+        "fullName":"Link",
+        "name":"link",
+        "mint":"5QSqnXtuiRxLafC1zqq4DqydFDzxsDjokCJSqF8hB5vZ"
     },
     {
-        fullName: 'SamoyedCoin',
-        name: 'samo',
-        mint: '3FnqS6QE4eiLWVGyMKT5H1h4u9xTub7Ex3HEcqmptiV4'
+        "fullName":"FTX Token",
+        "name":"ftt",
+        "mint":"A6TviEuVTijiihS4SbFS9Y9eZbT8PPk84fBQ4xuAbjJR"
     },
     {
-        fullName: 'Step Finance',
-        name: 'step',
-        mint: 'AwXdQ2aWs62hiTZjVLgF5UGeMfp3ujZJBqxfLM4DjGQY'
+        "fullName":"Binance Coin",
+        "name":"bnb",
+        "mint":"4C4HbpFFKrDjBYaRue6yxW5vT6d9Rz7baYXoB6MzXMFP"
     },
     {
-        fullName: 'Cope',
-        name: 'cope',
-        mint: 'B3Fs4FZJ2f3VYgoNpNZqFzy4Mk6hZFrZXXrTquiozaqZ'
+        "fullName":"Polkdot",
+        "name":"dot",
+        "mint":"21QeB8g5scwR1PbYoUfYJmkkx22Uu7fc1GPxgypWi7eK"
     },
     {
-        fullName: 'Bonfida',
-        name: 'fida',
-        mint: '8H5hGFNGayrzL9UBDwQS9VS9j6CP4yS2f6hXvNyFDCWB'
-    },
-    { 
-        fullName: 'Uniswap',
-        name: 'uni', 
-        mint: '7TWmp8n6M8RaSCFEB8dj6dYT299RneJ9QFRk3KMyA9aD' 
-    },
-    { 
-        fullName: 'Ethereum Classic',
-        name: 'etc', 
-        mint: 'GCgKYNGucy97cBeAvgSGoaibMihkFdsnkzCVTRJXo9D' 
-    },
-    { 
-        fullName: 'Stellar',
-        name: 'xlm', 
-        mint: 'DPdUr2oC3YsbPejtLooMNthpsWSZ5f6m9e4HQ1N8cgRt' 
-    },
-    { 
-        fullName: 'VeChain',
-        name: 'vet', 
-        mint: 'FMU7bF7RXAzyb2kHHnPpWQt1qG5yXoLyRJH7mKv4Tbpv' 
-    },
-    { 
-        fullName: 'Oxygen',
-        name: 'oxy', 
-        mint: '7qSZuhuMvcohMo8kbzfymJqh1NFkYc1A23CP3ip8Ux12' 
-    },
-    { 
-        fullName: 'Mercurial Finance',
-        name: 'mer', 
-        mint: '5LhM8FQ8oxsJMG2EEF8yeHZEeERbGdCZUf5KLMscDCDL' 
-    },
-    { 
-        fullName: 'yearn.finance',
-        name: 'yfi', 
-        mint: 'J1DNvob28HgpzukrdGqEGPdE5pRLBbtpvdwURyKU9Ymr' 
+        "fullName":"Bitcoin Cash",
+        "name":"bch",
+        "mint":"FL84oLhoNAJQQNVtSd3sJBLbN3ov5wxyGbe8UTiAD9ZR"
     },
     {
-        fullName: 'SafeMoon',
-        name: 'safemoon',
-        mint: 'HqMXoohEXbPCrVDcsDaM42wj5NiP6tZL4s3WvgSdq9qR'
+        "fullName":"Cardano",
+        "name":"ada",
+        "mint":"PmpH2ay2kcQdYdS4SsUdNe4KJfPJqQc2hCdzEQf4Avy"
     },
     {
-        fullName: '3X Long Bitcoin Token',
-        name: 'bull',
-        mint: 'Fcgo5xK2Td5SALSb4k93vmgm4MUXYuoak1PuvmNYzPq2'
+        "fullName":"Internet Computer",
+        "name":"icp",
+        "mint":"BVb7R64HHuKYPwkGhCUxQxxsxpzaUKcuxsDbMUDfLpbs"
     },
     {
-        fullName: '3X Short Bitcoin Token',
-        name: 'bear',
-        mint: '5ghXuJUkdJrzeGMSTLBMJq4WSm8Q63pcHU8nchNC4S6H'
+        "fullName":"PancakeSwap",
+        "name":"cake",
+        "mint":"88iaqzemdz7uBvCgp8YBGP9Hf394SuKbnGGDNRxRuGdH"
     },
     {
-        fullName: '3X Long Ethereum Token',
-        name: 'ethbull',
-        mint: 'GWNHKeRfQNK6pZaqBNjaboHgZhJRdBNVmx1Sp5xbSWew'
+        "fullName":"THORChain",
+        "name":"rune",
+        "mint":"2qFcXo8G7XkRXFPMP96shumCvjcae9jymonFudMCAeuW"
     },
     {
-        fullName: '1X Short Shitcoin Index Token',
-        name: 'hedgeshit',
-        mint: 'CKrkDRRT6YyQVBQsL1gFKtiDaMsXLrYSvTEPPLBjeTFt'
+        "fullName":"Dogecoin",
+        "name":"doge",
+        "mint":"At3cSnA8VicjcwvLe63RayUwmNc4sVxcSwLTAdNu7f16"
     },
     {
-        fullName: '3X Short Shitcoin Index Token',
-        name: 'bearshit',
-        mint: 'GXoJ5DuWGkQy9kdtBMNFmWsEQMKHDtNTGHeSFaTNmV1r'
+        "fullName":"Step Finance",
+        "name":"step",
+        "mint":"AwXdQ2aWs62hiTZjVLgF5UGeMfp3ujZJBqxfLM4DjGQY"
     },
     {
-        fullName: '0.5X Long Shitcoin Index Token',
-        name: 'halfshit',
-        mint: '6omMcBEBwvYDMw9JtRmEHPdhJa2AmPgYd6whc4yDABdS'
+        "fullName":"Cope",
+        "name":"cope",
+        "mint":"B3Fs4FZJ2f3VYgoNpNZqFzy4Mk6hZFrZXXrTquiozaqZ"
     },
     {
-        fullName: '3X Long Shitcoin Index Token',
-        name: 'bullshit',
-        mint: 'BUmCwTrWytEkmDBSMaat5iVGxT94fVmdeu28cTxYUA4M'
+        "fullName":"Bonfida",
+        "name":"fida",
+        "mint":"8H5hGFNGayrzL9UBDwQS9VS9j6CP4yS2f6hXvNyFDCWB"
     },
     {
-        fullName: '0.5X Long Dogecoin Token',
-        name: 'dogehalf',
-        mint: '526Gbo14LrXeTEpzi82wX1XFCXiS8m4wWP3zSVL3mZBP'
+        "fullName":"Uniswap",
+        "name":"uni",
+        "mint":"7TWmp8n6M8RaSCFEB8dj6dYT299RneJ9QFRk3KMyA9aD"
     },
     {
-        fullName: '1X Short Dogecoin Token',
-        name: 'dogehedge',
-        mint: '6cpV4HgrkQBWbh6w3JYNK2ZbSaT5Cv7n5vaHDKAyKatm'
+        "fullName":"Ethereum Classic",
+        "name":"etc",
+        "mint":"GCgKYNGucy97cBeAvgSGoaibMihkFdsnkzCVTRJXo9D"
+    },
+    {
+        "fullName":"Stellar",
+        "name":"xlm",
+        "mint":"DPdUr2oC3YsbPejtLooMNthpsWSZ5f6m9e4HQ1N8cgRt"
+    },
+    {
+        "fullName":"VeChain",
+        "name":"vet",
+        "mint":"FMU7bF7RXAzyb2kHHnPpWQt1qG5yXoLyRJH7mKv4Tbpv"
+    },
+    {
+        "fullName":"Oxygen",
+        "name":"oxy",
+        "mint":"7qSZuhuMvcohMo8kbzfymJqh1NFkYc1A23CP3ip8Ux12"
+    },
+    {
+        "fullName":"Mercurial Finance",
+        "name":"mer",
+        "mint":"5LhM8FQ8oxsJMG2EEF8yeHZEeERbGdCZUf5KLMscDCDL"
+    },
+    {
+        "fullName":"yearn.finance",
+        "name":"yfi",
+        "mint":"J1DNvob28HgpzukrdGqEGPdE5pRLBbtpvdwURyKU9Ymr"
+    },
+    {
+        "fullName":"3X Long Bitcoin Token",
+        "name":"bull",
+        "mint":"Fcgo5xK2Td5SALSb4k93vmgm4MUXYuoak1PuvmNYzPq2"
+    },
+    {
+        "fullName":"3X Long Ethereum Token",
+        "name":"ethbull",
+        "mint":"GWNHKeRfQNK6pZaqBNjaboHgZhJRdBNVmx1Sp5xbSWew"
+    },
+    {
+        "fullName":"1X Short Shitcoin Index Token",
+        "name":"hedgeshit",
+        "mint":"CKrkDRRT6YyQVBQsL1gFKtiDaMsXLrYSvTEPPLBjeTFt"
+    },
+    {
+        "fullName":"3X Long Shitcoin Index Token",
+        "name":"bullshit",
+        "mint":"BUmCwTrWytEkmDBSMaat5iVGxT94fVmdeu28cTxYUA4M"
+    },
+    {
+        "fullName":"1X Short Dogecoin Token",
+        "name":"dogehedge",
+        "mint":"6cpV4HgrkQBWbh6w3JYNK2ZbSaT5Cv7n5vaHDKAyKatm"
+    },
+    {
+        "fullName":"1X Short Altcoin Index Token",
+        "name":"althedge",
+        "mint":"4sXsJ4CQjxoZGiABEz23cZy5sCUtzboTEufZPQfMf5nS"
+    },
+    {
+        "fullName":"1X Short Bitcoin Token",
+        "name":"hedge",
+        "mint":"C3B4GaJySgRFVCPTe6Jm9JQkQVQmyFUZNoAWBBeCuMny"
+    },
+    {
+        "fullName":"3X Long Dogecoin Token",
+        "name":"dogebull",
+        "mint":"FvrExdK4WmimrToTLcG4Z3Q2Z7XwUf95cuFnbWGWa3pz"
+    },
+    {
+        "fullName":"3X Long Altcoin Index Token",
+        "name":"altbull",
+        "mint":"EJsQY6MQgLeRj7oDH6VeymqUK4ChtJ1pw1YWqGE3Vy2u"
     }
-]
+];
 
 export interface Token {
     fullName: string,
@@ -219,3 +199,101 @@ export function getToken(mint: string) {
         return "";
     }
 }
+
+export function logAllTokensInJson() {
+    let tokens: Token[] = [];
+
+    console.log('[\n');
+
+    mintKeys.forEach(key => {
+        console.log(JSON.stringify({
+            fullName: '',
+            name: key,
+            mint: mintObjs[key]
+        }));
+
+        console.log('\n');
+    });
+
+    console.log(']');
+
+    console.log();
+}
+
+const mintKeys = [
+    "usdc",
+    "btc",
+    "eth",
+    "xrp",
+    "sol",
+    "srm",
+    "sushi",
+    "link",
+    "ftt",
+    "bnb",
+    "dot",
+    "bch",
+    "ada",
+    "icp",
+    "cake",
+    "rune",
+    "doge",
+    "step",
+    "cope",
+    "fida",
+    "uni",
+    "etc",
+    "xlm",
+    "vet",
+    "oxy",
+    "mer",
+    "yfi",
+    "bull",
+    "ethbull",
+    "hedgeshit",
+    "bullshit",
+    "dogehedge",
+    "althedge",
+    "hedge",
+    "dogebull",
+    "altbull"
+];
+
+const mintObjs = {
+    "usdc": "2aqBRavq4UxeQys6ABue8zWxaJZAB9sbBRNFY3kX7p31",
+    "btc": "FYaRg2Di29JALi5B5YtAvQahJZLbZDVAFYW3JmpMrWW",
+    "eth": "AHEAwFN1qxoNMkcsRopYikgSRhiKkKUjcVux5qCBcxkj",
+    "xrp": "7yBAhJdvH8wtWp4mdPYNj1MPiCCdqDfG7ge4u9imPcnw",
+    "sol": "Csd6HUGPaenA3Kwhms2pm6pR3hJ7hdh4H2ejF9hKykkY",
+    "srm": "Pr3EmS5XYWwo8R28xXYQv3fLDudQFv9jvMmHsqHPwES",
+    "sushi": "GQQJXvQfzGUFfzJcLjkyM2Ziq3snmE4VFVcV2JUKcJrM",
+    "link": "5QSqnXtuiRxLafC1zqq4DqydFDzxsDjokCJSqF8hB5vZ",
+    "ftt": "A6TviEuVTijiihS4SbFS9Y9eZbT8PPk84fBQ4xuAbjJR",
+    "bnb": "4C4HbpFFKrDjBYaRue6yxW5vT6d9Rz7baYXoB6MzXMFP",
+    "dot": "21QeB8g5scwR1PbYoUfYJmkkx22Uu7fc1GPxgypWi7eK",
+    "bch": "FL84oLhoNAJQQNVtSd3sJBLbN3ov5wxyGbe8UTiAD9ZR",
+    "ada": "PmpH2ay2kcQdYdS4SsUdNe4KJfPJqQc2hCdzEQf4Avy",
+    "icp": "BVb7R64HHuKYPwkGhCUxQxxsxpzaUKcuxsDbMUDfLpbs",
+    "cake": "88iaqzemdz7uBvCgp8YBGP9Hf394SuKbnGGDNRxRuGdH",
+    "rune": "2qFcXo8G7XkRXFPMP96shumCvjcae9jymonFudMCAeuW",
+    "doge": "At3cSnA8VicjcwvLe63RayUwmNc4sVxcSwLTAdNu7f16",
+    "step": "AwXdQ2aWs62hiTZjVLgF5UGeMfp3ujZJBqxfLM4DjGQY",
+    "cope": "B3Fs4FZJ2f3VYgoNpNZqFzy4Mk6hZFrZXXrTquiozaqZ",
+    "fida": "8H5hGFNGayrzL9UBDwQS9VS9j6CP4yS2f6hXvNyFDCWB",
+    "uni": "7TWmp8n6M8RaSCFEB8dj6dYT299RneJ9QFRk3KMyA9aD",
+    "etc": "GCgKYNGucy97cBeAvgSGoaibMihkFdsnkzCVTRJXo9D",
+    "xlm": "DPdUr2oC3YsbPejtLooMNthpsWSZ5f6m9e4HQ1N8cgRt",
+    "vet": "FMU7bF7RXAzyb2kHHnPpWQt1qG5yXoLyRJH7mKv4Tbpv",
+    "oxy": "7qSZuhuMvcohMo8kbzfymJqh1NFkYc1A23CP3ip8Ux12",
+    "mer": "5LhM8FQ8oxsJMG2EEF8yeHZEeERbGdCZUf5KLMscDCDL",
+    "yfi": "J1DNvob28HgpzukrdGqEGPdE5pRLBbtpvdwURyKU9Ymr",
+    "bull": "Fcgo5xK2Td5SALSb4k93vmgm4MUXYuoak1PuvmNYzPq2",
+    "ethbull": "GWNHKeRfQNK6pZaqBNjaboHgZhJRdBNVmx1Sp5xbSWew",
+    "hedgeshit": "CKrkDRRT6YyQVBQsL1gFKtiDaMsXLrYSvTEPPLBjeTFt",
+    "bullshit": "BUmCwTrWytEkmDBSMaat5iVGxT94fVmdeu28cTxYUA4M",
+    "dogehedge": "6cpV4HgrkQBWbh6w3JYNK2ZbSaT5Cv7n5vaHDKAyKatm",
+    "althedge": "4sXsJ4CQjxoZGiABEz23cZy5sCUtzboTEufZPQfMf5nS",
+    "hedge": "C3B4GaJySgRFVCPTe6Jm9JQkQVQmyFUZNoAWBBeCuMny",
+    "dogebull": "FvrExdK4WmimrToTLcG4Z3Q2Z7XwUf95cuFnbWGWa3pz",
+    "altbull": "EJsQY6MQgLeRj7oDH6VeymqUK4ChtJ1pw1YWqGE3Vy2u"
+};
