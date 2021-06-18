c7b7b75c7db27e6ddcc7dd2df83bfd0b08b70a78
Willake Linode <willakelin@gmail.com>
2021-06-18 00:21:20 +0000

    setup server implementation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import * as TelegramBot from "node-telegram-bot-api";
 import { PublicKey } from "@solana/web3.js";
 import { config } from "dotenv";
-import { PrivateClient } from "./privateClient";
+import { GroupClient } from "./groupClient";
 
 config();
 
-const MY_FUND_ID = new PublicKey(process.env.MY_FUND_ID);
 const TARGET_FUND_ID = new PublicKey(process.env.TARGET_FUND_ID);
 
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
@@ -13,8 +12,7 @@ const CHAT_ID = process.env.CHAT_ID;
 
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: false });
 
-const client = new PrivateClient(
-  MY_FUND_ID,
+const client = new GroupClient(
   TARGET_FUND_ID,
   bot,
   CHAT_ID
